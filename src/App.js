2774cd0124f871fda887309424448c8ca304470d
Chris Skogstad <chris.skogstad1@gmail.com>
2020-12-22 01:28:45 +1100

    created API call to randomuser that generates 10 random employees in JSON format into the console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
+import API from "./utils/API";
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
 
 function App() {
+  const [employee, setEmployeeState] = useState({
+
+  })
+
+  useEffect(() => {
+    // For demonstration purposes, we mock an API call.
+    API.getUsers().then((res) => {
+      console.log(res)
+      //  setDeveloperState(res);
+      // console.log("Developer State:");
+      // console.log(developerState);
+    });
+  }, []);
+
+
+
   return (
     <>
       <Header />
       <Footer />
+
     </>
   )
 }
